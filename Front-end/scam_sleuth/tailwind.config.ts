8893dcfb617f856b5a7f630462914926143be666
L00Pz <mahbodseyedzadegan@gmail.com>
2024-11-16 12:23:29 +0330

    lalalallalaalalalalallalaalal

--- Front-end/scam_sleuth/tailwind.config.ts
+++ Front-end/scam_sleuth/tailwind.config.ts
@@ -23,9 +23,6 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		fontFamily: {
-  			sans: ['Montserrat']
-  		},
   		keyframes: {
 			shimmer: {
 				'0%': { backgroundPosition: '-200% 0' },
